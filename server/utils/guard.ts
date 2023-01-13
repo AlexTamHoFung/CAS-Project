b3a553eac75cbae6928fdd8f8e283078b12126fc
AlexT <alex.tam1927@gmail.com>
2023-01-13 14:50:56 +0800

    set up server env -2

--- server/utils/guard.ts
+++ server/utils/guard.ts
@@ -7,25 +7,25 @@ import { Bearer } from "permit";
 const permit = new Bearer({ query: "access_token" });
 
 export async function isLoggedIn(
-  req: Express.Request,
-  res: Express.Response,
-  next: Express.NextFunction
+	req: Express.Request,
+	res: Express.Response,
+	next: Express.NextFunction
 ) {
-  try {
-    const token = permit.check(req);
-    if (!token) {
-      return res.status(401).json({ msg: "Permission Denied" });
-    }
-    const payload = jwtSimple.decode(token, jwt.jwtSecret);
-    const user = await authService.getUserByUUID(payload.uuid);
-    if (user) {
-      const { password, ...others } = user;
-      req.user = { ...others };
-      return next();
-    } else {
-      return res.status(401).json({ msg: "Permission Denied" });
-    }
-  } catch (error) {
-    return res.status(401).json({ msg: "Permission Denied" });
-  }
+	try {
+		const token = permit.check(req);
+		if (!token) {
+			return res.status(401).json({ msg: "Permission Denied" });
+		}
+		const payload = jwtSimple.decode(token, jwt.jwtSecret);
+		const user = await authService.getUserByUUID(payload.uuid);
+		if (user) {
+			const { password, ...others } = user;
+			req.user = { ...others };
+			return next();
+		} else {
+			return res.status(401).json({ msg: "Permission Denied" });
+		}
+	} catch (error) {
+		return res.status(401).json({ msg: "Permission Denied" });
+	}
 }
