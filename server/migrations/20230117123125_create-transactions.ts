f6af49a952fc7719442836c10af3ef7e065a4cf2
Shawn Hui <huikalam911@gmail.com>
2023-01-20 16:16:33 +0800

    knex

---
+++ server/migrations/20230117123125_create-transactions.ts
@@ -0,0 +1,25 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable("transactions", function(table){
+        table.increments();
+        table.timestamp("transaction_date").notNullable;
+        table.integer("amount").notNullable;
+        table.string("payment_method").notNullable;
+        table.boolean("collect_point").notNullable;
+        table.boolean("is_refund").defaultTo(false).notNullable;
+        table.string("customer_id").notNullable;
+        table.foreign("customer_id").references("customers.id");
+        table.string("store_user_id").notNullable;
+        table.foreign("store_user_id").references("store_users.id");
+        table.timestamps(true, true);
+
+    });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable("transactions");
+}
+
---
+++ server/migrations/20230120080942_create-advertisements.ts
@@ -0,0 +1,22 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable("advertisements", function (table) {
+        table.increments();
+        table.string("advertisement_type").notNullable;
+        table.text("description").notNullable;
+        table.date("start_date").notNullable;
+        table.date("end_date").notNullable;
+        table.boolean("is_deleted").notNullable;
+        table.integer("company_id").notNullable;
+        table.foreign("company_id").references("companies.id");
+        table.timestamps(true, true);
+    });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable("advertisements");
+}
+
