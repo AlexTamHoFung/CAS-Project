6516e878cf198631e3f90cef7bfefcdd3103121d
AlexT <alex.tam1927@gmail.com>
2023-02-03 21:42:06 +0800

    fix merge bugs

--- frontend/src/views/user/Home/Home.tsx
+++ frontend/src/views/user/Home/Home.tsx
@@ -1,82 +1,82 @@
-import * as React from "react";
+import React from "react";
 import { useEffect, useState } from "react";
-
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 
 import Container from "@mui/material/Container";
+import { flexbox } from "@mui/system";
+// import Header from "../../../features/Header/Header";
 import BottomNav from "../../../features/BottomNav/BottomNav";
 import UserHeader from "../../../components/userheader/UserHeader";
-import dayjs from "dayjs";
 
-
-interface Companies {
+interface Coupon {
   id: number;
   name: string;
-  category: string;
+  description: string;
   coupon_type: string;
+  points_required: number;
+  valid_start: string;
+  valid_end: string;
+  company_id: number;
 }
-
-
 const { REACT_APP_API_BASE } = process.env;
+export default function Redeem() {
+  const [couponList, setCouponList] = useState<Coupon[]>([]);
 
-export default function Home() {
-  const [shopList, setShopList] = useState<Companies[]>([]);
-
-  // fetch here and update shopList
+  // fetch here and update couponList
   useEffect(() => {
-    fetch(`${REACT_APP_API_BASE}/companies/showCompany`)
+    fetch(`${REACT_APP_API_BASE}/listings/getListing`)
       .then((resp) => resp.json())
-      .then((data) => setShopList(data));
-  }, [shopList]);
-
+      .then((data) => setCouponList(data));
+  }, [couponList]);
   return (
-    <div className="App">
+    <div>
       <UserHeader />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+
       <Container fixed>
         {couponList.map((coupon) => (
           <Card key={`coupon_${coupon.id}`} style={{ marginBottom: 25 }}>
             <CardHeader
-              title={shop.name.toUpperCase()}
-              subheader={`Reuired Points: ${shop.name}`}
+              title={coupon.name.toUpperCase()}
+              subheader={`Reuired Points: ${coupon.points_required}`}
             />
             <button
-              style={{ backgroundColor: "rgb(0, 202, 0)", color: "white" }}
+              style={{  color: "black" }}
             >
-              Add to cart
+              Redeem
             </button>
+
 
             <CardMedia
               component="img"
               height="200"
-              image={`https://loremflickr.com/240/180?random=${shop.id}`}
+              image={`https://loremflickr.com/240/180?random=${coupon.id}`}
             />
             <CardContent>
               <Typography variant="body2" color="text.secondary">
-                Date: {shop.name} - {shop.name}
+                Date: {coupon.valid_start} - {coupon.valid_end}
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                {shop.name}
+                {coupon.description}
               </Typography>
             </CardContent>
           </Card>
         ))}
       </Container>
       <br />
-      <br />
-      <br />
-      <br />
-      <p>暫未有其他優惠</p>
       <br />
       <br />
       <br />
--- frontend/src/views/user/Record/Record.tsx
+++ frontend/src/views/user/Record/Record.tsx
@@ -6,12 +6,8 @@ import BottomNav from "../../../features/BottomNav/BottomNav";
 const Record = () => {
   return (
     <div>
-<<<<<<< HEAD
-      
-=======
       <UserHeader />
       <BottomNav />
->>>>>>> f52e670a3a671d475838a295bcfdcbadd94845fa
     </div>
   );
 }
