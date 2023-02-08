e853793baf32c3658ee0033becd4a01161e981a5
AlexT <alex.tam1927@gmail.com>
2023-02-08 17:37:54 +0800

    fix show pt

--- server/router/transactionsRoutes.ts
+++ server/router/transactionsRoutes.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import { transactionsController } from "../routes";
 import { isStoreLoggedIn } from "../utils/guard";
-import { isStoreLoggedIn } from "../utils/guard";
 import { asyncWrapper } from "../utils/wrapper";
 
 export const transactionsRoutes = express.Router();
