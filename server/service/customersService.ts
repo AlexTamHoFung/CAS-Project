ac7f60292781824d2734735ffe01bb2a5e770f95
AlexT <alex.tam1927@gmail.com>
2024-01-15 23:47:47 +0800

    change env

--- server/service/customersService.ts
+++ server/service/customersService.ts
@@ -1,23 +1,14 @@
 import { Knex } from "knex";
 import { ApplicationError } from "../utils/error";
-import {  hashPassword } from "../utils/hash";
+import { hashPassword } from "../utils/hash";
 import { Customer } from "./model";
 
 export class CustomersService {
 	constructor(private dbClient: Knex) {}
-
-	// async getCustomerByPhone(phone: number) {
-	// 	const getCustomer = await this.dbClient("customers")
-	// 		.select(phone)
-	// 		.where({ phone: phone });
-
-	// 	return getCustomer;
-	// }
-
 	async getCustomerIdByUUID(uuid: string) {
 		const getCustomerId = await this.dbClient("customers")
 			.select("id")
-			.where({uuid: uuid})
+			.where({ uuid: uuid })
 			.first();
 
 		return getCustomerId;
@@ -27,15 +18,21 @@ export class CustomersService {
 		name: string,
 		email: string,
 		password: string,
-		phone: number,
+		phone: number
 	) {
 		const customer = await this.dbClient("customers")
 			.where("email", "=", email)
 			.where("phone", "=", phone)
 			.first(["id", "uuid", "name", "email", "password", "phone"]);
 		if (!customer) {
-			password = await hashPassword(password);
-			const insertData = { name, email, password, phone };
+			const hashedPassword = await hashPassword(password);
+			const insertData = {
+				username: name,
+				name,
+				email,
+				password: hashedPassword,
+				phone
+			};
 			const result = await this.dbClient("customers")
 				.insert(insertData)
 				.returning("id");
@@ -44,14 +41,12 @@ export class CustomersService {
 		throw new ApplicationError("Duplicated User", 400);
 	}
 
-	async checkCustomer(email: string ) {
-		const customer = await this.dbClient.select("*")
-		.from<Customer>("customers")
+	async checkCustomer(email: string) {
+		const customer = await this.dbClient
+			.select("*")
+			.from<Customer>("customers")
 			.where({ email: email })
 			.first(["id", "uuid", "email", "password"]);
-			return customer;
-		// if (customer && (await checkPassword(password, customer.password))) {
-		// }
-		// throw new InvalidInfoError();
+		return customer;
 	}
 }
